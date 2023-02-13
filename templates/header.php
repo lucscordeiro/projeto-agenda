b303154b303bb4372f3ba23f4960ad982f6a7a4e
lucscordeiro <100317679+lucscordeiro@users.noreply.github.com>
2023-02-13 17:42:41 -0300

    exercitando 13.02

---
+++ templates/header.php
@@ -0,0 +1,40 @@
+<?php
+
+    include_once("config/url.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contatos</title>
+
+   
+
+    <!--PEGUEI NO https://cdnjs.com/-->
+    <!--BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!--FONT AWESOME-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+    <!--CSS-->
+    <link rel="stylesheet" href="<?= $BASE_URL?>css/styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+            </a>
+            <div>
+                <div class="navbar-nav">
+                    <a class="nav-link active" id="home-link"  href="<?= $BASE_URL ?>index.php">Agenda</a>
+                    <a class="nav-link active" id="create-link"  href="<?= $BASE_URL ?>create.php">Adicionar Contato</a>            
+                </div>
+            </div>
+        </nav>
+    </header>
